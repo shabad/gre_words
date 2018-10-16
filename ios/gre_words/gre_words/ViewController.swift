1387f7e8947cf932b319bec3dfb725bcc7cd6869
Shabad Sobti <shabad.sobti@gmail.com>
2018-10-16 14:51:17 -0400

    Base ios app

---
+++ ios/gre_words/gre_words/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gre_words
+//
+//  Created by Shabad Sobti on 10/16/18.
+//  Copyright © 2018 Shabad Sobti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
