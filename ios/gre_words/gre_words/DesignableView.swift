00d9463ccf5dd8190590b6da53a64a2dfba5ef5d
Shabad Sobti <shabad.sobti@gmail.com>
2018-10-17 11:55:08 -0400

    Basic Layouts

---
+++ ios/gre_words/gre_words/DesignableView.swift
@@ -0,0 +1,50 @@
+//
+//  DesignableView.swift
+//  gre_words
+//
+//  Created by Shabad Sobti on 10/16/18.
+//  Copyright © 2018 Shabad Sobti. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class DesignableView: UIView
+{
+    @IBInspectable var gradientColor1: UIColor = UIColor.white {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    @IBInspectable var gradientColor2: UIColor = UIColor.white {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    @IBInspectable var gradientStartPoint: CGPoint = .zero {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    @IBInspectable var gradientEndPoint: CGPoint = CGPoint(x: 0, y: 1) {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    private func setGradient()
+    {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [self.gradientColor1.cgColor, self.gradientColor2.cgColor]
+        gradientLayer.startPoint = self.gradientStartPoint
+        gradientLayer.endPoint = self.gradientEndPoint
+        gradientLayer.frame = self.bounds
+        if let topLayer = self.layer.sublayers?.first, topLayer is CAGradientLayer
+        {
+            topLayer.removeFromSuperlayer()
+        }
+        self.layer.addSublayer(gradientLayer)
+    }
+}
