3422d81a28b5123a6626d4bf4a26a44ae5a0a266
Shabad Sobti <shabad.sobti@gmail.com>
2018-10-18 14:51:04 -0400

    Basic Sockets functionalty

---
+++ ios/gre_words/gre_words/SocketIOManager.swift
@@ -0,0 +1,9 @@
+//
+//  SocketIOManager.swift
+//  gre_words
+//
+//  Created by Shabad Sobti on 10/18/18.
+//  Copyright © 2018 Shabad Sobti. All rights reserved.
+//
+
+import Foundation
--- server/app.py
+++ server/app.py
@@ -1,10 +1,32 @@
 from flask import Flask
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@socketio.on('connect')
+def on_connect():
+    print("Connectd")
+
+
+
+
+@socketio.on('join')
+def on_join(data):
+    name = data['name']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    name = data['name']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', room=room)
+
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
