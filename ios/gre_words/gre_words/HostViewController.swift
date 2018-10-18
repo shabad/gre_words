110c0c1fbc212c063a767c87cb80d730144fb7a6
Shabad Sobti <shabad.sobti@gmail.com>
2018-10-18 12:06:33 -0400

    View Controllers template

---
+++ ios/gre_words/gre_words/HostViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HostViewController.swift
+//  gre_words
+//
+//  Created by Shabad Sobti on 10/18/18.
+//  Copyright © 2018 Shabad Sobti. All rights reserved.
+//
+
+import UIKit
+
+class HostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
