f1da5f2076df5885bc5b85c4232f9c715ef64591
Saad Khan <saadkn@d-172-25-255-128.dhcp.virginia.edu>
2018-10-27 18:36:46 -0400

    Updated2

--- server/models.py
+++ server/models.py
@@ -6,16 +6,17 @@ db = SQLAlchemy(app)
 
 
 class GRE(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    Words = db.Column(db.String(20), nullable = False)
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    Word = db.Column(db.String(20), nullable = False)
     Meaning = db.Column(db.String(120), nullable = False) 
 
 
 class Users(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
+	id = db.Column(db.Integer, primary_key = True, autoincrement = True)
 	Username = db.Column(db.String(20), primary_key = True)
 	Name = db.Column(db.String(10), nullable = False)
 	Email = db.Column(db.String(30), nullable = False)
 	Password = db.Column(db.String(25), nullable = False)
-	Stats = db.Column(db.Integer, nullable = False)
-	WeakWords = db.Column(db.String(20), nullable = False)
+	Avatar = db.Column(db.String(130), nullable = True)
+
+
